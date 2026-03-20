ec68894dd6934cf00e7b129de7f6e87ce8ae8740
minjun0316 <mjkang040316@kookmin.ac.kr>
2026-03-20 21:57:24 +0900

    commit week2

---
+++ week2/password_using_string/password_using_string.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string password;
+
+    cout << "프로그램을 종료하려면 암호를 입력하세요." << endl;
+
+    while (true) {
+        cout << "암호>> ";
+        cin >> password;
+
+        if (password == "C++") {
+            cout << "프로그램을 정상 종료합니다." << endl;
+            break;
+        }
+        else {
+            cout << "암호가 틀립니다~" << endl;
+        }
+    }
+
+    return 0;
+}
