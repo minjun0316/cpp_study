acaff5dea2fa1e75a66a514226a87f875b30c2f6
minjun0316 <mjkang040316@kookmin.ac.kr>
2026-03-20 17:22:45 +0900

    add week3 exponential

---
+++ week3/class_exponential/class_exponential.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+class Exp {
+private:
+	int base;
+	int exp;
+
+public:
+	Exp() {
+		base = 1;
+		exp = 1;
+	}
+	Exp(int b) {
+		base = b;
+		exp = 1;
+	}
+	Exp(int b, int e) {
+		base = b;
+		exp = e;
+	}
+	int getBase() {
+		return base;
+	}
+	int getExp() {
+		return exp;
+	}
+	int getValue() {
+		int result = 1;
+		for (int i = 0; i < exp; i++) {
+			result *= base;
+		}
+		return result;
+	}
+	bool equals(Exp b) {
+		return getValue() == b.getValue();
+	}
+};
+
+int main() {
+	Exp a(3, 2);
+	Exp b(9);
+	Exp c;
+
+	cout << a.getValue() << ' ' << b.getValue() << ' ' << c.getValue() << endl;
+	cout << "base of a " << a.getBase() << ", exponential " << a.getExp() << endl;
+
+	if (a.equals(b))
+		cout << "same" << endl;
+	else
+		cout << "not same" << endl;
+}
