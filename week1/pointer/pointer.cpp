578dc76368fe53a3549ba047aff902a432670c65
minjun0316 <mjkang040316@kookmin.ac.kr>
2026-03-20 16:55:10 +0900

    init with gitignore

---
+++ week1/pointer/pointer.cpp
@@ -0,0 +1,19 @@
+﻿// pointer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int g = 20; /*전역 변수*/
+
+int add(int x, int y) {//전역 함수
+    return x+y;
+}
+int main()
+{
+    int a, b, sum; //지역 변수
+    cin >> a >> b; //입력
+    sum = a + b;
+    cout << sum; //출력
+    return 0;
+}
